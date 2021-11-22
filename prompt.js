9475451bf2bc6aacbdba496209537ef75d852785
RoyW08 <44249777+RoyW08@users.noreply.github.com>
2021-11-22 15:21:56 -0500

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,8 @@
+const passphrase = 'I Have spoken'; 
+console.log( passphrase.toUpperCase() );
+console.log(passphrase);
+
+//console.log( "This is my Luck".length );
+
+
+// prompt("What is your name?");
